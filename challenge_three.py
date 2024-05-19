5e88c8e2748aebbaee14ee69be5686d2783d6247
Zach Frankel <zachif1997@gmail.com>
2024-05-19 13:04:40 -0500

    Last minite changes

--- challenge_three.py
+++ challenge_three.py
@@ -45,18 +45,21 @@ peices=[
 #The issus is based on the above example output it seems I'm supposed to look for each word one at a time rather than using "find()" or something similar to check if each combination of peices is present in the list of words so thats what I'm gonna do, but if I'm iterating through the list of peices in order "albums" is gonna come up before "alber" so based on the example I'm gonna assume I'm supposed to check all words starting with "al" against "albums" even after I already find it in the list and by that reasoning I'm gonna assume I'm supposed to check all the words that start with "bar" against "barley" even after I've already found the combination that makes "barley". I'm also not sure if I'm supposed to be removing peices and words from the list as I go, but based on the example I'm gonna assume I don't
 
 #this iterator will keep track of the current word I'm searching for even after finding it per the example
-iii=0
-#this is the first loop
-for i in peices:
-    #this is the second loop
-    for ii in peices:
-        #this is just a variable to hold the current concatination
-        currentString = i + ii
-        #checks if the concatination matches the word
-        if currentString == words[iii]:
-            print(f"{currentString} == {words[iii]}")
-        else:
-            print(f"{currentString} != {words[iii]}")
-    #this increments through the list of "words" after every two "peices" in the outer loop to produce the expected output based on the instructions and example
-    if (peices.index(i) + 1) % 2 == 0:
-        iii = iii+1 
+def wordPeices(words, peices):
+    iii=0
+    #this is the first loop
+    for i in peices:
+        #this is the second loop
+        for ii in peices:
+            #this is just a variable to hold the current concatination
+            currentString = i + ii
+            #checks if the concatination matches the word
+            if currentString == words[iii]:
+                print(f"{currentString} == {words[iii]}")
+            else:
+                print(f"{currentString} != {words[iii]}")
+        #this increments through the list of "words" after every two "peices" in the outer loop to produce the expected output based on the instructions and example
+        if (peices.index(i) + 1) % 2 == 0:
+            iii = iii+1 
+
+wordPeices(words, peices)
